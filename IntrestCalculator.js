6f23e50b838b699ba567ffc476416d5ac4707b13
Durga <gollanagadurga16@gmail.com>
2023-10-12 14:41:12 +0530

    intrest calculator

--- IntrestCalculator.js
+++ IntrestCalculator.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
-import { Grid } from "@mui/material";
+import { Grid, Paper } from "@mui/material";
 import Box from "@mui/material/Box";
 import Input from "@mui/material/Input";
 import InputLabel from "@mui/material/InputLabel";
@@ -15,97 +15,130 @@ export default function Intrestcalculator() {
   const [time, setTime] = useState(1);
   const [totalintrest, setTotalintrest] = useState(0);
   const [totalamount, setTotalamount] = useState(0);
+  const [timeFrame, setTimeFrame] = useState("month");
+  const selectHandler = (event) => {
+    setTimeFrame(event.target.value);
+  };
+
   const calculateHandler = () => {
-    const intrest = principal * rate * time;
+    const currentTime = timeFrame === "year" ? time * 12 : time;
+    const intrest = principal * rate * currentTime;
     setTotalintrest(intrest / 100);
     setTotalamount(intrest / 100 + principal);
   };
+
   return (
-    <Box
-      style={{
-        border: "1px solid black",
-        width: 500,
-        height: 600,
-        marginLeft: 400,
-      }}
-    >
-      <h1>Simple Intrest Calculator</h1>
-      <Grid container style={{ marginTop: 50 }}>
-        <Grid item xs={6}>
-          <FormControl variant="standard">
-            <label style={{ paddingRight: 150 }}>Principal:</label>
-            <TextField
-              id="standard-basic"
-              variant="standard"
-              onChange={(event) => setPrincipal(Number(event.target.value))}
-            />
-          </FormControl>
-        </Grid>
-        <Grid item xs={6}>
-          <FormControl variant="standard">
-            <label style={{ paddingRight: 150 }}>Rate:</label>
-            <TextField
-              id="standard-basic"
-              variant="standard"
-              onChange={(event) => setRate(Number(event.target.value))}
-              value={rate}
-            />
-          </FormControl>
-        </Grid>
-      </Grid>
-
-      <Grid container style={{ paddingLeft: 25, marginTop: 30 }}>
-        <Grid item xs={4}>
-          <FormControl variant="standard">
-            <label style={{ paddingRight: 150 }}>Time:</label>
-            <TextField
-              id="standard-basic"
-              variant="standard"
-              onChange={(event) => setTime(Number(event.target.value))}
-            />
-          </FormControl>
-        </Grid>
-
-        <Grid item xs={8} style={{ paddingRight: 50 }}>
-          <FormControl variant="standard">
-            <InputLabel htmlFor="component-simple"></InputLabel>
-            <Select label="year">
-              <MenuItem value="year">year</MenuItem>
-              <MenuItem value="month">month</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-      </Grid>
-
-      <Button
-        variant="contained"
-        style={{ backgroundColor: "#01e26e", marginRight: 50, marginTop: 50 }}
-        onClick={calculateHandler}
-      >
-        Calculate
-      </Button>
-
-      <Grid container>
-        <List
-          sx={{ width: "100%", maxWidth: 460, marginLeft: 1 }}
-          component="nav"
-          aria-labelledby="nested-list-subheader"
-          style={{ background: "#72ffb4" }}
+    <Grid container>
+      <Grid item xs={6}>
+        <Paper
+          elevation={2}
+          style={{
+            margin: 80,
+            width: 500,
+            hight: 300,
+            borderRadius: 5,
+            background: "white",
+            padding: 20,
+          }}
         >
-          <ListItem>
-            <ListItemText id="princepal" primary="Principal Amount :" />
-            <p>{principal}</p>
-          </ListItem>
-          <ListItem>
-            <ListItemText id="total" primary="Total Interest :" />
-            <p>{totalintrest}</p>
-          </ListItem>
-          <ListItem>
-            <ListItemText id="t amount" primary="Total Amount :" />
-            <p>{totalamount}</p>
-          </ListItem>
-        </List>
+          <Grid container style={{ marginTop: 50 }}>
+            <Grid item xs={6}>
+              <FormControl variant="standard">
+                <label style={{ paddingRight: 150 }}>Principal:</label>
+                <TextField
+                  id="standard-basic"
+                  variant="standard"
+                  onChange={(event) => setPrincipal(Number(event.target.value))}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl variant="standard">
+                <label style={{ paddingRight: 150 }}>Rate:</label>
+                <TextField
+                  id="standard-basic"
+                  variant="standard"
+                  onChange={(event) => setRate(Number(event.target.value))}
+                  value={rate}
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+
+          <Grid container style={{ paddingLeft: 25, marginTop: 30 }}>
+            <Grid item xs={4}>
+              <FormControl variant="standard">
+                <label style={{ paddingRight: 150 }}>Time:</label>
+                <TextField
+                  id="standard-basic"
+                  variant="standard"
+                  onChange={(event) => setTime(Number(event.target.value))}
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={8} style={{ paddingRight: 50 }}>
+              <FormControl variant="standard">
+                <InputLabel htmlFor="component-simple"></InputLabel>
+                <Select label="year" onChange={selectHandler} value={timeFrame}>
+                  <MenuItem value="year">year</MenuItem>
+                  <MenuItem value="month">month</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+
+          <Button
+            variant="contained"
+            style={{
+              backgroundColor: "#01e26e",
+              marginRight: 50,
+              marginTop: 50,
+              marginBottom: 20,
+            }}
+            onClick={calculateHandler}
+          >
+            Calculate
+          </Button>
+
+          <Grid container>
+            <List
+              sx={{ width: "100%", maxWidth: 460, marginLeft: 1 }}
+              component="nav"
+              aria-labelledby="nested-list-subheader"
+              style={{ background: "#00ff7b" }}
+            >
+              <ListItem>
+                <ListItemText id="princepal" primary="Principal Amount :" />
+                <p>{principal}</p>
+              </ListItem>
+              <ListItem>
+                <ListItemText id="total" primary="Total Interest :" />
+                <p>{totalintrest}</p>
+              </ListItem>
+              <ListItem>
+                <ListItemText id="t amount" primary="Total Amount :" />
+                <p>{totalamount}</p>
+              </ListItem>
+            </List>
+          </Grid>
+        </Paper>
       </Grid>
-    </Box>
+      <Grid item xs={6} style={{ marginTop: 150 }}>
+        <h2 style={{ color: "white", fontSize: 60 }}>SIMPLE INTEREST</h2>
+        <h1 style={{ fontSize: 65 }}>CALCULATOR</h1>
+        <h3
+          style={{
+            background: "white",
+            width: 300,
+            marginLeft: 200,
+            padding: 10,
+            fontSize: 40,
+          }}
+        >
+          REACT&JS
+        </h3>
+      </Grid>
+    </Grid>
   );
 }
